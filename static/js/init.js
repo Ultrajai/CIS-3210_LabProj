f47f19ceecd52022ca5a4037514f78291693f88e
ajai <ajai@uoguelph.ca>
2020-10-01 20:15:05 -0400

    almost done lab 3

--- static/js/init.js
+++ static/js/init.js
@@ -3,6 +3,12 @@
 $(document).ready(function () {
 
   console.log("Ajai Gill 1015577")
+  console.log("Information about sql injection and input sanitization:")
+  console.log("One way you can prevent sql injection is to use prepared statements with parameterized queries. parameterized queries force devs to define sql code then pass in each parameter into the query. This allows the database to distinguish between code and data. Prepared statements prevent hackers from being able to change the intent of a query even if they manage to submit sql commands.")
+  console.log("Another way you can prevent sql injection is to use stored procedures. This is very similar to parameterized queries but with stored procedures the sql commands are stored in the database and then called by the application. This is as good as parameterized queries.")
+  console.log("Another way is to validate input or input sanitization. Validation is focusing on the fact that if inputs by the user target certain tables or columns that the code should catch this. the input should be processed in the front end before it reaches the database. Smart coding is needed for input validation to work. input sanitization is a little different where you look for aspects of the user input and remove the code, if there is any. This is best approached with tools already developed because it can be very difficult on your own. It is not guarunteed to work everytime in every scenario.");
+  console.log("There are more ways to prevent attack but the best ways are to use prepared statements with parameterized queries or stored procedures.")
+  console.log("this information was obtained at https://cheatsheetseries.owasp.org/cheatsheets/SQL_Injection_Prevention_Cheat_Sheet.html")
 
 	$("#PostUser").click(function(event){
 
